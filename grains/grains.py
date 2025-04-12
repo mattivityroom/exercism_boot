9faf8ee03e37cd2188c29c0fce3a20cbd2d2bd19
mattivityroom <mattivityroom@gmail.com>
2025-04-12 09:52:01 +0700

    [finish] grains

--- grains/grains.py
+++ grains/grains.py
@@ -1,6 +1,10 @@
 def square(number):
-    pass
-
+    if ( number <= 0 or number > 64 ):
+        raise ValueError("square must be between 1 and 64")
+    return 2 ** (number-1)
 
 def total():
-    pass
+    total = 0
+    for i in range (64):
+        total += square(i+1)
+    return total

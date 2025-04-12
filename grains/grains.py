76dbbcd16c6a505a496e693b25362d00e060930a
mattivityroom <mattivityroom@gmail.com>
2025-04-12 10:16:10 +0700

    [refactor] grains using bitshift operator

--- grains/grains.py
+++ grains/grains.py
@@ -3,7 +3,7 @@ LIMIT = 64
 def square(number):
     if ( number <= 0 or number > LIMIT ):
         raise ValueError("square must be between 1 and 64")
-    return 2 ** (number-1)
+    return 1 << number - 1
 
 def total():
-    return 2 ** 64 - 1
+    return (1 << 64) - 1

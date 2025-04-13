602afc2d780c53f69e0226b87541f5161d35b00f
mattivityroom <mattivityroom@gmail.com>
2025-04-13 14:44:48 +0700

    [refactor] armstrong-numbers using sum with itterator object

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -1,11 +1,2 @@
 def is_armstrong_number(number):
-    str_number = str(number)
-    total = 0
-    for i in str_number:
-        total = total + ( int(i) ** len(str_number))
-
-    if total == number:
-        return True
-    return False
-
-
+    return number  == sum ( int(i) ** len(str(number)) for i in str(number) )

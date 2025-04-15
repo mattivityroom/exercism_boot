a3284eebe253b52601b75b64fa1a49bf874aa240
mattivityroom <mattivityroom@gmail.com>
2025-04-16 06:42:24 +0700

    [refactor] triangle.py

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -10,8 +10,14 @@ def equilateral(sides):
 
 
 def isosceles(sides):
-    return (sides[0] == sides[1] or sides[0] == sides[2] or sides[1] == sides[2]) \
-        and not is_violation(sides)
+    if not is_violation(sides):
+        return (sides[0] == sides[1] or sides[0] == sides[2] or sides[1] == sides[2])
+    else:
+        return False
 
 def scalene(sides):
-    return not isosceles(sides) and not is_violation(sides)
+    # return not isosceles(sides) and not equilateral(sides)
+    if not is_violation(sides):
+        return not isosceles(sides) and not equilateral(sides)
+    else:
+        return False

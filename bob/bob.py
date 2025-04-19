1647b1f0d954a1d0baf5d15a2d15a718e8ea8155
mattivityroom <mattivityroom@gmail.com>
2025-04-19 08:27:50 +0700

    [finish] bob exercise

--- bob/bob.py
+++ bob/bob.py
@@ -1,2 +1,14 @@
 def response(hey_bob):
-    pass
+    if hey_bob.isupper() and "?" in hey_bob:
+        return 'Calm down, I know what I\'m doing!'
+
+    if hey_bob.isupper():
+        return 'Whoa, chill out!'
+
+    if hey_bob.strip().endswith('?'):
+        return 'Sure.'
+
+    if hey_bob.strip() == "":
+        return 'Fine. Be that way!'
+
+    return 'Whatever.'

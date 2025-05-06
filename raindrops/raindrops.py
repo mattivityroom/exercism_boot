291b1b555d6400110026c9b6b01445a5832c4a3c
mattivityroom <mattivityroom@gmail.com>
2025-05-06 07:54:19 +0700

    [finish] raindrops

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,2 +1,18 @@
 def convert(number):
-    pass
+    rain_sound = ""
+
+    if (number % 3 == 0):
+        rain_sound += "Pling"
+
+    if (number % 5 == 0):
+        rain_sound += "Plang"
+
+    if (number % 7 == 0):
+        rain_sound += "Plong"
+
+    if rain_sound == "":
+        return str(number)
+    else:
+        return rain_sound
+
+    # return str(number)
